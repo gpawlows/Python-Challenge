947d4dee99c584d4441057be1da37cbb9ce5ed69
Geoff Pawlowski <gpawlows@gmail.com>
2021-05-30 15:25:47 -0700

    git commit -m

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,11 @@
 import os
 import csv
+
+#create empty lists
+voterID = []
+county = []
+candidate = []
+votes = 0
 # Set path for file
 
 csvpath = os.path.join('Resources/election_data.csv')
@@ -8,3 +14,25 @@ csvpath = os.path.join('Resources/election_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+    #Read csv header
+    csv_header = next(csvfile)
+
+    #fill in Lists with data from csv file
+    for row in csvreader:
+        #Add voterIDs
+        voterID.append(row[0])
+        #Add counties
+        county.append(row[1])
+        #Add candidate
+        candidate.append(row[2])
+        #increment vote count
+        votes += 1
+    print(len(voterID))
+    print(votes)
+    
+    #reset lists
+    voterID = []
+    county = []
+    candidate = []
+
